f4449e642d16cf5b201258490bef3dbcce053ed6
step- <step-@users.noreply.github.com>
2020-08-18 16:44:57 +0200

    Improve gron test script.

--- lib/test-js-dot-path/with-gron.sh
+++ lib/test-js-dot-path/with-gron.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+export AWK_BIN=${AWK_BIN:-/aufs/pup_ro/usr/bin/gawk --posix}
+# gawk
+# busybox awk
+# mawk
+
 die () { echo "$@" >&2; exit 1; }
 
 json_awk () {
-	awk -v _JS_VAR_VAR=null -f ../js-dot-path.awk -f ../../JSON.awk -v STREAM=0 "$@"
+	$AWK_BIN -v _JS_VAR_VAR=null -f ../js-dot-path.awk -f ../../JSON.awk -v STREAM=0 "$@"
 }
 
 REDIR=/dev/null
@@ -12,17 +17,15 @@ case $1 in
 	--show-stderr ) shift; REDIR=/dev/stderr ;;
 esac
 
-! [ -f "$1" ] && die "usage: ${BASH_SOURCE##*/} [--show-stderr] JSON_FILE"
+! [ -f "$1" ] && die "usage: [AWK_BIN=command] ${BASH_SOURCE##*/} [--show-stderr] JSON_FILE"
 
 gron --no-sort "$@" | sort > /tmp/gron.out
 
 json_awk "$@" 2> $REDIR | sort > /tmp/json_awk.out
 
+#meld /tmp/{gron,json_awk}.out &
 diff -y /tmp/{gron,json_awk}.out
 status=$?
-
-#meld /tmp/{gron,json_awk}.out &
-
 [ 0 != ${PIPESTATUS[0]} ] && less "$@"
 
 exit $status
